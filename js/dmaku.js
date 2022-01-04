f645d4fb85c6a548d1be284abe39e12eb32d513e
13270411202 <13270411202@163.com>
2022-01-04 11:00:49 +0800

    1

--- js/dmaku.js
+++ js/dmaku.js
@@ -8,6 +8,15 @@ var bigbooms = [];
 
 window.onload = function(){
 	initAnimate()
+	setInterval("toggleSound()",100);
+}
+
+function toggleSound() {
+	   var music = document.getElementById("vd");//获取ID  
+	   if (music.paused) { //判读是否播放  
+		   music.paused=false;
+		   music.play(); //没有就播放 
+	   }    
 }
 
 function initAnimate(){
